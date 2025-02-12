b198dd7beee59d72ba236b612a0656821aa926fe
RachitShahrawat <rachitshahrawat@gmail.com>
2025-02-12 16:11:56 +0530

    setup rabbit mq

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ const {PORT}=require('./config/serverconfig');
 // const {sendBasicEmail}=require('./services/email-service');
 const TicketController=require('./controllers/ticket-controller');
 const jobs=require('./utils/job');
+const {createChannel}=require('./utils/messageQueue')
 
-const setupAndStartServer=()=>{
+const setupAndStartServer=async ()=>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+  // const channel=await createChannel();
 
     app.post('/api/v1/tickets',TicketController.create);
 
     app.listen(PORT,()=>{
      console.log(`Server started at port ${PORT}`);
-     jobs();
+    //  jobs();
 
     //  sendBasicEmail(
     //     'support@admin.com',
